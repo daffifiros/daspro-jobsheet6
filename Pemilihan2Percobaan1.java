d08c538efb472b672594b5ca0e2a2cac0cf88f20
daffifiros <daffikun10@gmail.com>
2024-10-03 14:01:56 +0700

    Percobaan 2

--- Pemilihan2Percobaan1.java
+++ Pemilihan2Percobaan1.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan1 {
 
     public static void main(String[] args) {
-        Scanner Absen = new Scanner(System.in);
-        System.out.print("Masukkan nomor absen: ");
-        int inputAbsen = Absen.nextInt();
-        System.out.print("Masukan tahun: ");
-        int tahun = Absen.nextInt();
+        Scanner input17 = new Scanner(System.in);
+        System.out.print("Masukkan tahun: ");
+        int tahun = input17.nextInt();
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
