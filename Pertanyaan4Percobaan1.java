0d9b1b86fca9902fa29484adf2c57260d13fd6e8
daffifiros <daffikun10@gmail.com>
2024-10-04 08:38:25 +0700

    Percobaan

---
+++ Pertanyaan4Percobaan1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pertanyaan4Percobaan1 {
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+
+        // Meminta input dari user
+        System.out.print("Masukkan tahun: ");
+        int tahun = input17.nextInt();
+
+        // Mengecek apakah tahun kabisat atau bukan
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("tahun kabisat");
+                } else {
+                    System.out.println("bukan tahun kabisat");
+                }
+            } else {
+                System.out.println("tahun kabisat");
+            }
+        } else {
+            System.out.println("bukan tahun kabisat");
+        }
+    }
+}
