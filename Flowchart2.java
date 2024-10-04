a0bae85e96691d89c4d7d0971175d8afc3482344
daffifiros <daffikun10@gmail.com>
2024-10-04 10:38:32 +0700

    Hasil Tugas

---
+++ Flowchart2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Flowchart2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String jenisBuku;
+        int jumlahBuku;
+
+        System.out.print("Masukkan jenis buku: ");
+        jenisBuku = scanner.nextLine();
+        System.out.print("Masukkan jumlah buku: ");
+        jumlahBuku = scanner.nextInt();
+
+        if (jenisBuku.equalsIgnoreCase("Kamus") && jumlahBuku >= 3) {
+            System.out.println("Diskon 5%");
+        } else if (jenisBuku.equalsIgnoreCase("Kamus") && jumlahBuku >= 2) {
+            System.out.println("Diskon 12%");
+        } else if (jenisBuku.equalsIgnoreCase("Novel") && jumlahBuku >= 3) {
+            System.out.println("Diskon 9%");
+        } else if (jenisBuku.equalsIgnoreCase("Novel") && jumlahBuku >= 2) {
+            System.out.println("Diskon 8%");
+        } else {
+            System.out.println("Diskon 0%");
+        }
+    }
+}
