625e6d5002565f001a715abde10ada12012dec0e
daffifiros <daffikun10@gmail.com>
2024-10-04 07:54:33 +0700

    Hasil Percobaan

--- Pemilihan2Percobaan117.java
+++ Pemilihan2Percobaan117.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pemilihan2Percobaan1 {
+public class Pemilihan2Percobaan117 {
 
     public static void main(String[] args) {
         Scanner input17 = new Scanner(System.in);
---
+++ TugasJobsheet6.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class TugasJobsheet6 {
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner (System.in);
+    }
+}
